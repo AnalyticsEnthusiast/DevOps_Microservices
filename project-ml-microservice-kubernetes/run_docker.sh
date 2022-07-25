f9219c4ebe82423aee05b7e3a8a2da3dc6337bc1
EC2 Default User <ec2-user@ip-172-31-7-179.ec2.internal>
2022-07-25 18:45:50 +0000

    Dockerfile and run script created

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build -t house-prediction-ml .
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+docker run -d --rm -p 8000:80 --name house-pred-1 house-prediction-ml
