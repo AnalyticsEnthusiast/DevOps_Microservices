3878059877ac2a0252309ec6fdd0d24838f9e0b2
Darren Foley <darrenfoley015@gmail.com>
2022-07-25 19:12:08 +0000

    updated app for logging, upload docker script created

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -3,13 +3,20 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
+USERNAME="theanalyst6"
+IMAGE_NAME="house-prediction-ml"
+TAG="v1.0"
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=${USERNAME}/${IMAGE_NAME}:${TAG}
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+
+docker tag ${IMAGE_NAME}:latest ${dockerpath}
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
